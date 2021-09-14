e1b011e9e09fc533e5391c3c470544966ced756b
yeongroksong <yeongroksong@yeongroksongui-MacBookPro.local>
2021-09-14 16:42:53 +0900

    Add: Django Project Setting

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'django-insecure-()9s65nf%#o9g1tgawi*=w3j$tz9-diw0!q@r0^wqop-wicfh-'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westarbucks',
+        'USER': 'root',
+        'PASSWORD': 'song1992',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
